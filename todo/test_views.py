b7e142f8c58b4665efb475a4d57df960caafab6c
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-08-12 09:18:01 +0000

    Django

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,8 +1,30 @@
 from django.test import TestCase
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.asserttemplateUsed(response, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        item = Item.object.create(name='Test Todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.asserttemplateUsed(response, 'todo/add_item.html')
+
+    def test_get_edit_item_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.asserttemplateUsed(response, 'todo/edit_item.html')
+    
+    def test_can_add_item(self):
+        response = self.client.post('/add', )
+    
+    def test_can_delete_item(self):
+    
+    def test_can_toggle_item(self):
+
+
 
 
